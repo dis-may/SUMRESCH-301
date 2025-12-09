64a0c86955f68b0831ce071d7dbbc1c4d3943354
May Gan <119158272+dis-may@users.noreply.github.com>
2025-12-09 16:28:20 +1300

    experiment with ball code

--- ball.py
+++ ball.py
@@ -5,13 +5,13 @@ import matplotlib.pyplot as plt
 # ---------------------------
 # Physical parameters
 # ---------------------------
-g = 9.81
-e = 0.8
+g = 9.81 # gravity
+e = 0.8 # elsaticity of bounce
 bounce_height = 0.2   # <-- PLATFORM HEIGHT
-y0 = 1.0
-v0 = 0.0
+y0 = 1.0 # initial height
+v0 = 0.0 # initial velocity
 t0 = 0.0
-tf = 10.0
+tf = 2
 eps = 1e-8
 max_bounces = 100
 
@@ -19,17 +19,20 @@ max_bounces = 100
 # ODE system
 # ---------------------------
 def dynamics(t, state):
-    y, v = state
+    y, v = state # height, velocity
     return [v, -g]
 
 # ---------------------------
 # Event: hit platform at y = 0.2
 # ---------------------------
 def hit_platform(t, state):
-    return state[0] - bounce_height
-
+    return state[0] - bounce_height # when this is negative, then, the event fires
 hit_platform.terminal = True
-hit_platform.direction = -1
+
+def woohoo(t, state):
+    return state[0] - 0.5
+woohoo.terminal = True
+# hit_platform.direction = -1
 
 # ---------------------------
 # Simulation loop
@@ -43,6 +46,7 @@ y_all = []
 v_all = []
 
 impact_times = []
+other_event_times = []
 
 while t_current < tf and bounces < max_bounces:
 
@@ -50,7 +54,7 @@ while t_current < tf and bounces < max_bounces:
         dynamics,
         (t_current, tf),
         state_current,
-        events=hit_platform,
+        events=[hit_platform, woohoo],
         max_step=0.02,
         rtol=1e-7,
         atol=1e-9
@@ -60,8 +64,11 @@ while t_current < tf and bounces < max_bounces:
     y_all.append(sol.y[0])
     v_all.append(sol.y[1])
 
+    print(sol.t_events)
+
     # If impact detected
-    if sol.t_events[0].size > 0:
+    if sol.t_events != None and sol.t_events[0].size > 0:
+
         t_impact = sol.t_events[0][0]
         v_pre = sol.y[1, -1]
 
@@ -74,9 +81,16 @@ while t_current < tf and bounces < max_bounces:
         # Restart slightly above platform
         t_current = t_impact + eps
         state_current = np.array([bounce_height + v_post * eps, v_post])
+    
+    elif sol.t_events != None and sol.t_events[1].size > 0:
+        print("SOMETHING")
+        other_event_times.append(sol.t_events[1][0])
+        state_current = [0.5, -sol.y[1, -1]]
+        continue
 
     else:
         break
+    
 
 # ---------------------------
 # Combine data
@@ -92,6 +106,7 @@ plt.figure(figsize=(10, 4.5))
 plt.plot(t_all, y_all)
 plt.axhline(bounce_height, linestyle="--")  # platform height
 plt.scatter(impact_times, [bounce_height]*len(impact_times), marker="x")
+plt.scatter(other_event_times, [0.5]*len(other_event_times), marker="x")
 plt.xlabel("Time (s)")
 plt.ylabel("Height (m)")
 plt.title("Ball Bouncing on a Platform at y = 0.2 m")
